7f994c6c5221b04bc06e767fe0d523bff53c11ea
mizuki830 <mizuki.lovebaske.0830@ezweb.ne.jp>
2020-10-28 12:51:55 +0900

    コントローラーの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,26 @@
 class RoomsController < ApplicationController
 
   def index
+    @rooms = Room.order('created_at DESC')
+  end
+
+  def new
+    @room = RoomTag.new
+  end
+
+  def create
+    @room = RoomTag.new(room_params)
+    if @room.valid?
+      @room.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room_tag).permit(:name, :comic, :agenda, :tag).merge(user_id: current_user.id)
   end
 end
